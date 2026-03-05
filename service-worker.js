9705c5296a5b92413502b47dae8540a692528155
jaynebianca47-png <jaynebianca47@gmail.com>
2026-03-04 23:13:53 -0300

    Create service-worker.js file with offline caching code

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+// Service Worker code for offline caching 
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/index.html',
+                '/styles.css',
+                '/script.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = ['my-cache'];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
